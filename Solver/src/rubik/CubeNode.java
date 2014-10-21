ec365abe8c60ab0b3d56bccf2154d0577840f36b
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2014-10-20 20:46:43 -0400

    debugging encoding

---
+++ Solver/src/rubik/CubeNode.java
@@ -0,0 +1,30 @@
+package rubik;
+
+import java.util.HashMap;
+
+public class CubeNode {
+
+	/**
+	 * The state of the cube
+	 */
+
+	public  HashMap<Integer,char[]>  state;
+	/**
+	 * The heuristic value
+	 */
+	public int heuristic;
+
+	/**
+	 *
+	 * @param state the state of the cube
+	 * @param heuristic the heuristic value
+	 */
+	public CubeNode( HashMap<Integer,char[]>  state, int heuristic) {
+		this.state = state;
+		this.heuristic = heuristic;
+	}
+
+
+
+	
+}
